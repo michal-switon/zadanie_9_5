326bb5ba141e477cf6d09b30433a0e68fc2d3afe
Michał Świtoń <michal.switon@op.pl>
2018-01-01 23:56:24 +0100

    new repository, buttons in index.html and script in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var withButtonItemClass = document.getElementsByClassName("button");
+
+    for (var i = 0; i < withButtonItemClass.length; i++) {
+        var text = withButtonItemClass[i].innerText;
+        console.log(text);
+    };
